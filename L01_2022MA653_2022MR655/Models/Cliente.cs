59809b6daed70b1ded24b7baa397443fbed87ebd
Juan Monroy <juan.monroy@catolica.edu.sv>
2025-02-18 18:01:29 -0600

    Agregando clase cliente y motorista en models

---
+++ L01_2022MA653_2022MR655/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace L01_2022MA653_2022MR655.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+
+        public string? NombreCliente { get; set; }
+
+        public string? Direccion { get; set; }
+    }
+}
---
+++ L01_2022MA653_2022MR655/Models/Motorista.cs
@@ -0,0 +1,9 @@
+﻿namespace L01_2022MA653_2022MR655.Models
+{
+    public class Motorista
+    {
+        public int MotoristaId { get; set; }
+
+        public string? NombreMotorista { get; set; }
+    }
+}
